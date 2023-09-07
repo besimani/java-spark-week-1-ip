1ce57de149bb55f7df8a2a81d6a689ad1daa738f
Bernard Amaitsa <besimani@gmail.com>
2023-09-08 02:21:32 +0300

    update src/main/java/ke/co/safaricom/javasparkweek1ip/Cipher.java

--- src/main/java/ke/co/safaricom/javasparkweek1ip/Cipher.java
+++ src/main/java/ke/co/safaricom/javasparkweek1ip/Cipher.java
@@ -3,6 +3,7 @@ package ke.co.safaricom.javasparkweek1ip;
 public class Cipher {
     private String message;
     private int key;
+    public String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public Cipher() {
     }
@@ -22,12 +23,21 @@ public class Cipher {
     public void setKey(int key) {
         this.key = key;
 
-    public string encrypt(){
-        return "";
+    public String encrypt(){
+        char[] message = this.getMessage().toCharArray();
+        String output = "";
+        for (int i = 0; i < message.length; 1++){
+            char c = message[i];
+            int charPos = this.alphabets.indexOf(c);
+            int newPos = charPos + this.getKey();
+            char newChar = this.alphabets.charAt(newPos);
+            output = output + newChar;
+        }
+        return output;
     }
 
-        public string decrypt(){
-            return "";
+    public String decrypt(){
+        return "decrypt";
     }
 
     }
